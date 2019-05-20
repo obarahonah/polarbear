2c2bf65b0b9020e277c00330e73f63b57d6198c6
sergio <sergiolizano1111@gmail.com>
2019-05-20 00:25:49 -0600

    enemigo letal agregado

---
+++ Assets/scripts/enemies_behaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class enemies_behaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Rigidbody2D rb2d;
+    public float speed;
+
+    CapsuleCollider2D self_coll;
+
+
+    public GameObject bear;
+    public int mindistance;
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        self_coll = GetComponent<CapsuleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         moveDirection = new Vector3(0,0,0);
+
+    }
+}
