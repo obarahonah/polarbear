c55f3c711de15b782ee456f3aaba081151f03be5
sergio <sergiolizano1111@gmail.com>
2019-06-05 16:25:13 -0600

    transitions between levels

---
+++ Assets/scripts/levels_behaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class levels_behaviour : MonoBehaviour
+{
+
+    public string LevelName;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(LevelName);
+        }
+    }
+}
